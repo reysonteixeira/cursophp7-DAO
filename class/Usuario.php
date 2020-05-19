a40f54cc721d3aba90875fb201434cff3a951337
Reyson Teixeira Terra <reysonteixeira@gmail.com>
2020-05-19 13:15:14 -0300

    Metodos de lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -59,6 +59,34 @@
             }
         }
 
+        public static function getList(){
+            $sql = new Sql();
+            return ($sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin"));
+        }
+
+        public static function search($login){
+            $sql = new Sql();
+            return($sql->select("SELECT * FROM tb_usuarios WHERE deslogin LIKE :search ORDER BY deslogin",
+            array(':search'=>"%".$login."%")));
+        }
+
+        public function login($login, $password){
+            $sql = new Sql();
+            $result = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :login AND dessenha = :password", array(":login"=>$login, ":password"=>$password));
+
+            if(count($result) > 0){
+                $row = $result[0];
+
+                $this->setIdusuario($row['idusuario']);
+                $this->setDeslogin($row['deslogin']);
+                $this->setDessenha($row['dessenha']);
+                $this->setDtcadastro($row['dtcadastro']);
+            }
+            else{
+                throw new Exception("Login e/ou senha invalidos!");
+            }
+        }
+
         public function __toString(){
             return json_encode(array(
                 "idusuario"=> $this->getIdusuario(),
--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
     require_once("config.php");
 
-    $root = new Usuario();
+    //Carrega um usuario
+    //$root = new Usuario();
+    //$root->loadById(1);
+    //echo($root);
 
-    $root->loadById(1);
-    
-    echo($root);
+    //Carrega uma lista de usuarios
+    //$lista = Usuario::getList();
+   // echo json_encode($lista);
 
+   //Carrega uma lista de usuarios buscando pelo login
+    //$search = Usuario::search("rey");
+    //echo(json_encode($search));
+
+
+    //Carrega usuario usando login e a senha
+    $usuario = new Usuario();
+    $usuario->login("reysontt@gmail.com","reyson123456");
+    echo($usuario);
 ?>
